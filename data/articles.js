5e0433cec563d2ef053b7dc429386e9660e7bb59
John Martin <jrmartin182@gmail.com>
2020-04-08 11:35:30 -0600

    implemented read arcticles and DAL

--- data/articles.js
+++ data/articles.js
@@ -5,14 +5,40 @@ const assert = require('assert');
 const url = process.env.DB_URL;
 
 // Database Name
-const dbName = 'myproject';
+const dbName = 'wiki';
+const colName = 'articles';
  
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
+//Database settings
+const settings = { useUnifiedTopology: true };
+
+const getArticles = () => {
+    const iou = new Promise((resolve, reject) => {
+        // Use connect method to connect to the server
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err){
+                // assert.equal(null, err);
+                reject(err);
+            } else {
+                console.log("Connected successfully to server to GET artciles");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.find({}).toArray(function(err, docs) {
+                    if(err){
+                        // assert.equal(null, err);
+                        reject(err);
+                    } else {
+                        console.log("Found the following articles");
+                        console.log(docs);
+                        resolve(docs)
+                        client.close();
+                    }
+                  });
+            };
+        });
+    }) 
+    return iou;
+}
+
+module.exports = {
+    getArticles
+}
