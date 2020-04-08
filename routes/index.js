e0350c6aa2cefe871bec1589337b2eecdf6f8f0e
John Martin <jrmartin182@gmail.com>
2020-04-08 16:55:10 -0600

    built out views and linked to get artciles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const { 
+  getArticles
+} = require('../data/articles');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Catalog' });
+});
+
+router.get('/articles', async function(req, res, next){
+  try {
+    const articleList =  await getArticles()
+    res.render('articles', {title: 'Articles', data: articleList})
+  } catch(err){
+    reject(err);
+  }
+});
+ 
+router.get('/videos', function(req, res, next){
+  res.render('videos', {title: 'Videos'})
+});
+
+router.get('/podcasts', function(req, res, next){
+  res.render('podcasts', {title: 'Podcats'})
 });
 
 module.exports = router;
+
